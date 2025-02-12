f81e8fe6d8171b2b586151190e31bab58ffcd4c3
Pedro Melchior de Oliveira <pedro.melchiordeoliveira@gmail.com>
2025-02-12 08:42:42 -0400

    commit

--- atividade_avaliativa_31-01/ex3/excel.py
+++ atividade_avaliativa_31-01/ex3/excel.py
@@ -1,47 +1,46 @@
-import openpyxl
+import openpyxl as op
+import random
 
 # Exercício 6
 
-import random
-from builtins import range
 
-# wb = op.Workbook()
-# ws = wb.active
-# ws.title = 'Vendas'
-# ws.append(['Produto', 'Quantidade', 'Preço'])
-# produtos = ['Linguiça', 'Desodorante', 'Shampoo', 'Queijo', 'Manteiga']
-# for row in range(2, 7):
-#     ws.cell(row, 1, produtos[row - 2])
-#     ws.cell(row, 2, value=random.randint(1,20))
-#     ws.cell(row, 3). value=random.randint(10,17)
+wb = op.Workbook()
+ws = wb.active
+ws.title = 'Vendas'
+ws.append(['Produto', 'Quantidade', 'Preço'])
+produtos = ['Linguiça', 'Desodorante', 'Shampoo', 'Queijo', 'Manteiga']
+for linha in range(2, 7):
+    ws.cell(linha, 1, produtos[linha - 2])
+    ws.cell(linha, 2, value=random.randint(1,20))
+    ws.cell(linha, 3, value=random.randint(10,17))
 
-# wb.save('exemplo.xlsx')
+wb.save('atividade_avaliativa_31-01/ex3/dados.xlsx')
 
 
 # Exercício 7
 
 
-# wb = op.load_workbook('exemplo.xlsx')
-# ws = wb.active
+wb = op.load_workbook('atividade_avaliativa_31-01/ex3/dados.xlsx')
+ws = wb.active
 
-# for row in ws.iter_rows(values_only=True):
-#     print(row)
+for row in ws.iter_rows(values_only=True):
+    print(row)
 
 
 # Exercício 8
 
-# aux_a = op.Workbook()
-# aux = aux_a.active
-# aux.title = 'Vendas Filtradas'
+aux_a = op.Workbook()
+aux = aux_a.active
+aux.title = 'Vendas Filtradas'
 
-# wb = op.load_workbook('exemplo.xlsx')
-# ws = wb.active
-# line = 1
-# for row in ws.iter_rows(min_row=2, max_row=ws.max_row, values_only=True):
-#     if int(row[1]) * int(row[2]) > 100:
-#         aux.cell(line, 1, value=row[0])
-#         aux.cell(line, 2, value=row[1])
-#         aux.cell(line, 3, value=row[2])
-#         line += 1
+wb = op.load_workbook('atividade_avaliativa_31-01/ex3/dados.xlsx')
+ws = wb.active
+line = 1
+for row in ws.iter_rows(min_row=2, max_row=ws.max_row, values_only=True):
+    if int(row[1]) * int(row[2]) > 100:
+        aux.cell(line, 1, value=row[0])
+        aux.cell(line, 2, value=row[1])
+        aux.cell(line, 3, value=row[2])
+        line += 1
 
-# aux_a.save('vendas_filtradas.xlsx')
+aux_a.save('atividade_avaliativa_31-01/ex3/vendas_filtradas.xlsx')
