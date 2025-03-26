80e55dc081cf2df6cc6c0a5881ea0483e8113abf
Pedro Melchior de Oliveira <pedro.melchiordeoliveira@gmail.com>
2025-03-26 10:31:42 -0400

    commit

---
+++ nocoes_de_banco_de_dados/atividade_avaliativa_21-03/funcoes.py
@@ -0,0 +1,12 @@
+def menu():
+    print('========================================\n')
+    print('Bem vindo ao seu Restaurante Favorito')
+    print('1 - Adicionar Prato')
+    print('2 - Remover Prato')
+    print('3 - Gerenciar Pedidos')
+    print('4 - Cardápio')
+    print('5 - Fazer Pedido')
+    print('0 - Sair/Gerar Relatório Diário\n')
+    print('========================================\n')
+
+menu()
--- nocoes_de_banco_de_dados/atividade_avaliativa_21-03/index.py
+++ nocoes_de_banco_de_dados/atividade_avaliativa_21-03/index.py
@@ -1,14 +1,66 @@
 import sqlite3
-
+from funcoes import *
 conexao = sqlite3.connect('nocoes_de_banco_de_dados/atividade_avaliativa_21-03/dados.db')
 cursor = conexao.cursor()
 
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS Pratos (
-                   
-               
-               
-               )
-
-
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        Nome TEXT NOT NULL,
+        Valor FLOAT,
+        Tempo_Espera INTEGER
+        )
 ''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Clientes (
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        Nome TEXT NOT NULL,
+        Email TEXT NOT NULL       
+    )
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Pedidos (
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        Cliente_ID INTEGER,
+        Endereço TEXT NOT NULL,
+        Valor FLOAT,
+        Status TEXT NOT NULL,
+        Tempo_Entrega INTEGER,
+        FOREIGN KEY (Cliente_ID) REFERENCES Clientes(ID)
+    )
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Avaliacoes (
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        Pedido_ID INTEGER,
+        Nota FLOAT,
+        FOREIGN KEY (Pedido_ID) REFERENCES Pedidos(ID)
+    )
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Relatorios (
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        Vendas FLOAT,
+        Data TEXT NOT NULL           
+        FOREIGN KEY (Admin_ID) REFERENCES Admin(ID)   
+    )
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Prato_Pedido (
+        ID INTEGER PRIMAY KEY AUTOINCREMENT,
+        Prato_ID INTEGER,
+        Pedido_ID INTEGER
+    )
+''')
+
+conexao.commit()
+
+
+
+while True:
+    menu()
