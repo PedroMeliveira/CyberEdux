1760c91bbd7e0c2ff2ae2ab346480061ca5791e8
Pedro Melchior de Oliveira <pedro.melchiordeoliveira@gmail.com>
2025-03-28 14:55:52 -0400

    Delete teste.py

--- teste.py
+++
@@ -1,6 +0,0 @@
-import csv
-
-with open("saida.csv", "w", newline="") as arquivo:
-    escritor = csv.writer(arquivo)
-    escritor.writerow(["Nome", "Idade", "Cidade"])
-    escritor.writerow(["João", 30, "São Paulo"])
