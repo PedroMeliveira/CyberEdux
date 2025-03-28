92af245a1b33b26550e47c4ca436bdd3200c4038
Mateus A. Muniz S. <mateuszinum@gmail.com>
2025-03-28 10:30:50 -0400

    commit

--- nocoes_de_banco_de_dados/atividade_avaliativa_21-03/funcoes.py
+++ nocoes_de_banco_de_dados/atividade_avaliativa_21-03/funcoes.py
@@ -36,13 +36,24 @@ def try_float_positivo(msg):
 def try_int_positivo(msg):
     while True:
         try:
-            num_int = int(input(f'\n{msg}'))
-            if 0 > num_int:
-                print(f'\nDigite um valor positivo!')
+            num_int = input(f'\n{msg}')
+
+            if num_int.lower() == 'sair':
+                return num_int.lower()
+            
             else:
-                return num_int
+                num_int = int(num_int)
+
+                if 0 > num_int:
+                    print(f'\nDigite um valor positivo!')
+                else:
+
+                    return num_int
         except:
             print(f'\nEntrada inválida, tente novamente!')
+
+def opcao_sair():
+    print(f"\nDigite 'Sair' para voltar.")
 
 # ===========================================================================================
 
@@ -93,14 +104,13 @@ def cria_tabelas_sql():
         CREATE TABLE IF NOT EXISTS Relatorios (
             ID INTEGER PRIMARY KEY AUTOINCREMENT,
             Vendas FLOAT,
-            Data TEXT NOT NULL           
-            FOREIGN KEY (Admin_ID) REFERENCES Admin(ID)   
+            Data TEXT NOT NULL              
         )
     ''')
 
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS Prato_Pedido (
-            ID INTEGER PRIMAY KEY AUTOINCREMENT,
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
             Prato_ID INTEGER,
             Pedido_ID INTEGER
         )
@@ -111,9 +121,80 @@ def cria_tabelas_sql():
 
 
 def adicionar_prato():
+    opcao_sair()
+    
     nome = input(f'\nNome do prato: ')
+    
+    if nome.lower() != 'sair':
 
-    valor = try_float_positivo('Preço do prato: ') 
+        valor = try_float_positivo('Preço do prato: ') 
 
-    tempo_espera = try_int_positivo('Tempo de espera: ')
+        tempo_espera = try_int_positivo('Tempo de espera: ')
 
+        cursor.execute(f"INSERT INTO Pratos (Nome, Valor, Tempo_Espera) VALUES ('{nome}', '{valor}', '{tempo_espera}')")
+
+        conexao.commit()
+
+        print(f'\nPrato adicionado com sucesso! ')
+
+def remover_prato():
+    while True:
+        opcao_sair()
+
+        id = try_int_positivo(f'ID do prato a ser removido: ')
+
+        if str(id) == 'sair':
+            break
+
+        cursor.execute(f'SELECT ID, Nome FROM Pratos')
+
+        pratos = cursor.fetchall()
+
+        achou_prato = False
+
+        for prato in pratos:
+            if id == prato[0]:
+                while True:
+                    escolha = try_int_positivo(f'Deseja remover o Prato: {prato[0]} - {prato[1]}? [(1) - Sim, (2) - Não]: ')
+                    
+                    if escolha == 1:
+                        cursor.execute(f"DELETE FROM Pratos WHERE ID = {id}")
+
+                        conexao.commit()
+                        
+                        print(f'\nPrato removido com sucesso! ') 
+
+                        break
+                    
+                    elif escolha == 2:
+                        print(f'\nOk, prato NÃO foi removido. \n')
+
+                        break
+
+                    else:
+                        print(f'\nDigite uma opção válida. ')
+            
+            achou_prato = True
+
+        if not achou_prato:
+            print(f'\nEsse ID não está nos registros de pratos. Tente novamente! ')
+        
+        else:
+            break
+
+
+def insert_cardapio_fixo():
+    cursor.execute(f"INSERT INTO Pratos (Nome, Valor, Tempo_Espera) VALUES ('Hambúrguer Artesanal', '25.90', '15')")
+    cursor.execute(f"INSERT INTO Pratos (Nome, Valor, Tempo_Espera) VALUES ('Pizza Marguerita', '39.90', '20')")
+    cursor.execute(f"INSERT INTO Pratos (Nome, Valor, Tempo_Espera) VALUES ('Lasanha à Bolonhesa', '34.50', '25')")
+    cursor.execute(f"INSERT INTO Pratos (Nome, Valor, Tempo_Espera) VALUES ('Frango Grelhado com Legumes', '28.00', '20')")
+    cursor.execute(f"INSERT INTO Pratos (Nome, Valor, Tempo_Espera) VALUES ('Risoto de Cogumelos', '32.00', '30')")
+
+    cursor.execute(f"INSERT INTO Bebidas (Nome, Valor, Tempo_Espera) VALUES ('Refrigerante (Coca-Cola)', '6.00', '0')")
+    cursor.execute(f"INSERT INTO Bebidas (Nome, Valor, Tempo_Espera) VALUES ('Suco Natural de Laranja', '8.00', '0')")
+    cursor.execute(f"INSERT INTO Bebidas (Nome, Valor, Tempo_Espera) VALUES ('Água Mineral (500ml)', '3.50', '0')")
+
+    cursor.execute(f"INSERT INTO Sobremesas (Nome, Valor, Tempo_Espera) VALUES ('Sorvete de Chocolate', '12.00', '5')")
+    cursor.execute(f"INSERT INTO Sobremesas (Nome, Valor, Tempo_Espera) VALUES ('Pudim de Leite Condensado', '10.00', '10')")
+
+    conexao.commit()
--- nocoes_de_banco_de_dados/atividade_avaliativa_21-03/main.py
+++ nocoes_de_banco_de_dados/atividade_avaliativa_21-03/main.py
@@ -2,11 +2,15 @@ from funcoes import *
 
 cria_tabelas_sql()
 
+insert_cardapio_fixo()
+
 while True:
     menu()
 
     opcao = escolha(0, 5)
 
     if opcao == 1:
+        adicionar_prato()
 
-        
+    elif opcao == 2:
+        remover_prato()
