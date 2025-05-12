cc0b9d7a2831e984bd6af49f2aa29c04d2917252
Elisa C Medeiros <pedro.melchiordeoliveira@gmail.com>
2025-05-12 09:02:05 -0400

    commit

--- programação_orientada_a_objetos/atividade_avaliativa_01/main.py
+++ programação_orientada_a_objetos/atividade_avaliativa_01/main.py
@@ -9,6 +9,16 @@ class Livro:
         self.__disponivel = True
         self.categoria = categoria
     
+    def get_disponibilidade(self):
+        return self.__disponivel
+    
+    def set_disponibilidade(self, boolean):
+        self.__disponivel = boolean
+    
+    def get_isbn(self):
+        return self.__isbn
+    
+    
     def emprestar(self):
         if self.__disponivel == True:
             self.__disponivel = False
@@ -26,7 +36,8 @@ class Livro:
     def detalhes(self):
         print(f"{self.titulo} - {self.autor} - {self.ano} - {self.categoria}")
         
-        
+
+
 class Usuario:
     def __init__(self, nome):
         self.nome = nome
@@ -34,16 +45,19 @@ class Usuario:
         self.__livros_emprestados = []
         self.__limite = 0
         
+    def get_livros_emprestados(self):
+        return self.__livros_emprestados
+    
     def pegar_emprestado(self, livro, biblioteca):
-        if livro in biblioteca.__livros:
-            if livro.__disponivel and self.__limite < 3:
+        if livro in biblioteca.get_livros():
+            if livro.get_disponibilidade() and self.__limite < 3:
                 self.__livros_emprestados.append(livro)
                 self.__limite += 1
-                livro.__disponivel = False
-                biblioteca.__historico.append(livro)
+                livro.set_disponibilidade(False)
+                biblioteca.set_historico(livro)
                 print("Livro emprestado com sucesso!\n")
                 
-            elif not livro.__disponivel:
+            elif not livro.get_disponibilidade():
                 print("Livro não disponível!\n")
             
             else:
@@ -56,12 +70,12 @@ class Usuario:
         if livro in self.__livros_emprestados:
             self.__livros_emprestados.remove(livro)
             self.__limite -= 1
-            livro.__disponivel = True
+            livro.set_disponibilidade(True)
             print("Livro devolvido com sucesso!\n")
         
         else:
             print("Livro não esta na sua lista de livros emprestados!\n")
-            
+
             
 class Biblioteca:
     def __init__(self, nome):
@@ -69,22 +83,36 @@ class Biblioteca:
         self.__livros = []
         self.__usuarios = []
         self.__historico = []
-        
+    
+    def get_livros(self):
+        return self.__livros
+    
+    
+    def get_usuarios(self):
+        return self.__usuarios
+    
+    
+    def get_historico(self):
+        return self.__historico
+    
+    def set_historico(self, livro):
+        self.__historico.append(livro)
+    
     def adicionar_livro(self, livro):
         unico = True
         for liv in self.__livros:
-            if livro.__isbn == liv.__isbn:
+            if livro.get_isbn() == liv.get_isbn():
                 unico = False
                 print("Não foi possível adicionar, pois ja existe esse livro na biblioteca!\n")
                 break
         
         if unico:
             self.__livros.append(livro)
-            print("Livro adicionado com sucesso!\n")
+            print(f"Livro adicionado ({livro.titulo}) com sucesso!\n")
             
     def buscar_livro(self, termo):
         for livro in self.__livros:
-            if termo == livro.titulo or termo == livro.__isbn or termo == livro.autor:
+            if termo == livro.titulo or termo == livro.get_isbn() or termo == livro.autor:
                 return livro
         return None
             
@@ -97,5 +125,30 @@ class Biblioteca:
 
              
     def salvar_dados(self):
-        with open('biblioteca_dados.csv', 'w') as csvfile:
+        with open('programação_orientada_a_objetos/atividade_avaliativa_01/biblioteca_dados.csv', 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(self.__historico)
             
+bib_01 = Biblioteca('Cyber Edux')
+
+usuario_01 = Usuario('Pedro')         
+usuario_02 = Usuario('Mateus')    
+     
+livro_01 = Livro('Harry Potter', 'Jack Rowling', 2008, 'Ficção')
+livro_02 = Livro('Percy Jackson 1', 'Rick Riordan', 2010, 'Ficção')
+livro_03 = Livro('Percy Jackson 2', 'Rick Riordan', 2011, 'Ficção')
+livro_04 = Livro('Percy Jackson 3', 'Rick Riordan', 2012, 'Ficção')
+
+bib_01.adicionar_livro(livro_01)
+bib_01.adicionar_livro(livro_02)
+bib_01.adicionar_livro(livro_03)
+bib_01.adicionar_livro(livro_04)
+
+bib_01.registrar_usuario(usuario_01)
+
+usuario_01.pegar_emprestado(livro_01, bib_01)
+usuario_01.pegar_emprestado(livro_02, bib_01)
+usuario_01.pegar_emprestado(livro_03, bib_01)
+usuario_02.pegar_emprestado(livro_04, bib_01)
+
+bib_01.salvar_dados()
