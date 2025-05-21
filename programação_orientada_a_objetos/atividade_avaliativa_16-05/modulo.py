39fd8c03218d14a325e576465630a85a0aae1639
Elisa C Medeiros <pedro.melchiordeoliveira@gmail.com>
2025-05-21 10:14:59 -0400

    commit

--- programação_orientada_a_objetos/atividade_avaliativa_16-05/modulo.py
+++ programação_orientada_a_objetos/atividade_avaliativa_16-05/modulo.py
@@ -20,16 +20,38 @@ class Credito():
         self.validade = validade
         self.cvv = cvv
 
+
+def validar_cartao(numero):
+    regex_cartao = r'^\d{13,19}$'   
+    return re.fullmatch(regex_cartao, numero.replace(" ", "")) is not None
+
+def validar_cvv(cvv):
+    return re.fullmatch(r'^\d{3,4}$', cvv) is not None
+
 def cria_credito():
-    numero = st.number_input("Número do cartão: ", min_value=1000000000000000, max_value=9999999999999999)
+    numero = st.text_input("Número do cartão de crédito:")
+    
+    if numero:
+        if validar_cartao(numero):
+            st.success("✅ Número de cartão válido!")
+        else:
+            st.error("❌ Número inválido. Digite apenas números (13 a 19 dígitos).")
     
     nome = st.text_input("Nome do titular: ")
     
-    validade = st.date_input("Data de validade: ", format='DD/MM/YYYY', min_value='20/05/2025')
+    validade = st.date_input("Data de validade: ", format='DD/MM/YYYY', min_value='today')
     
-    cvv = st.number_input("CVV: ", min_value=100, max_value=999)
+    cvv = st.text_input("CVV: ", max_chars=4)
     
-    return Credito(numero, nome, validade, cvv)
+    if cvv:
+        if validar_cvv(cvv):
+            st.success("✅ CVV válido!")
+            
+        else:
+            st.error("❌ CVV inválido. Deve conter 3 ou 4 dígitos numéricos.")
+    
+    if validar_cartao(numero) and validar_cvv(cvv):
+        return Credito(numero, nome, validade, cvv)
     
 
 class Paypal():
@@ -46,18 +68,33 @@ def validar_email(email):
     regex_email = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
     return re.match(regex_email, email) is not None
 
+
+def validar_senha(senha):
+    regex_senha = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$'
+    return re.fullmatch(regex_senha, senha) is not None
+
+
 def cria_paypal():
-    
     email = st.text_input("Email: ")
+    
     if email:
         if validar_email(email):
             st.success("✅ E-mail válido!")
+            
         else:
             st.error("❌ E-mail inválido. Tente novamente.")
 
-    senha = st.text_input("Qual a senha: ")
+    senha = st.text_input("Senha: ", type='password')
     
-    return Paypal(email, senha)
+    if senha:
+        if validar_senha(senha):
+            st.success("✅ Senha válida!")
+            
+        else:
+            st.error("❌ A senha deve ter:\n- Mínimo 8 caracteres\n- Letra maiúscula e minúscula\n- Número\n- Caractere especial")
+    
+    if validar_email(email) and validar_senha(senha):
+        return Paypal(email, senha)
     
     
 class Bancaria():
@@ -65,15 +102,43 @@ class Bancaria():
         self.codigo = codigo
         self.origem = origem
         self.destino = destino
-        
+
+def validar_codigo_banco(codigo):
+    return re.fullmatch(r'^\d{3}$', codigo) is not None
+
+def validar_conta(conta):
+    return re.fullmatch(r'^\d{4,12}(-\d{1})?$', conta) is not None
+
 def cria_bancaria():
-    codigo = input("Qual o código do banco? \n")
+    codigo = st.text_input("Ccódigo do banco: ", max_chars=3)
     
-    origem = input("Qual a conta de origem? \n")
+    if codigo:
+        if validar_codigo_banco(codigo):
+            st.success("✅ Código de banco válido!")
+        else:
+            st.error("❌ Código inválido. Deve conter exatamente 3 dígitos numéricos.")
     
-    destino = input("Qual a conta de destino? \n")
+    origem = st.text_input("Conta de origem: ")
     
-    return Credito(codigo, origem, destino)
+    if origem:
+        if validar_conta(origem):
+            st.success("✅ Conta válida!")
+        
+        else:
+            st.error("❌ Conta inválida. Deve ter de 4 a 12 dígitos, com DV opcional (ex: 123456-7).")
+    
+    destino = st.text_input("Conta de destino: ")
+    
+    if destino:
+        if validar_conta(destino):
+            st.success("✅ Conta válida!")
+        
+        else:
+            st.error("❌ Conta inválida. Deve ter de 4 a 12 dígitos, com DV opcional (ex: 123456-7).")
+    
+    if validar_codigo_banco(codigo) and validar_conta(origem) and validar_conta(destino):
+        return Credito(codigo, origem, destino)
+
     
 def menu():
     st.set_page_config(page_title='Sistema de Pagamentos', layout='centered')
@@ -83,7 +148,7 @@ def menu():
 
     st.subheader('Adicionar Pagamento')
 
-def cria_pagamento():
+def cria_trans():
 
     col1, col2, col3 = st.columns(3)
     with col1:
@@ -98,13 +163,87 @@ def cria_pagamento():
     st.markdown("---")
     return Pagamento(metodo, valor, data)
 
+def cria_tabela():
+    conexao = sqlite3.connect('dados.db')
+    cursor = conexao.cursor()
+    
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Pagamentos (
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            Metodo TEXT NOT NULL,
+            Valor FLOAT,
+            Data TEXT NOT NULL,
+            Status TEXT NOT NULL
+        )
+       ''')
+    
+    conexao.commit()
+    
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Cartão_Crédito (
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            Número_Cartão TEXT NOT NULL,
+            Nome TEXT NOT NULL,
+            Validade TEXT NOT NULL,
+            CVV TEXT NOT NULL,
+            Pagamento_ID INTEGER,
+            FOREIGN KEY (Pagamento_ID) REFERENCES Pagamentos(ID)
+        )
+       ''')
+    
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Paypal (
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            Email TEXT NOT NULL,
+            Senha TEXT NOT NULL,
+            Pagamento_ID INTEGER,
+            FOREIGN KEY (Pagamento_ID) REFERENCES Pagamentos(ID)
+        )           
+        ''')
+    
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Trans_Bancária (
+            ID INTEGER PRIMARY KEY AUTOINCREMENT,
+            Código_Banco TEXT NOT NULL,
+            Conta_Origem TEXT NOT NULL,
+            Conta_Destino TEXT NOT NULL,
+            Pagamento_ID INTEGER,
+            FOREIGN KEY (Pagamento_ID) REFERENCES Pagamentos(ID)
+        )         
+        ''')
+    
+    conexao.commit()
+    
+def insert_pagamento_retorna_id(trans):
+    conexao = sqlite3.connect('dados.db')
+    cursor = conexao.cursor()
+    
+    cursor.execute('INSERT INTO Pagamentos (Metodo, Valor, Data, Status) VALUES (?, ?, ?, ?)', (trans.metodo, trans.valor, trans.data, trans.status))
+    
+    conexao.commit()
+    
+    return cursor.lastrowid
 
+def insert_credito(pagamento, pag_id):
+    conexao = sqlite3.connect('dados.db')
+    cursor = conexao.cursor()
     
-
-
+    cursor.execute('INSERT INTO Cartão_Crédito (Número_Cartão, Nome, Validade, CVV, Pagamento_ID) VALUES (?, ?, ?, ?, ?)', (pagamento.numero, pagamento.nome, pagamento.validade, pagamento.cvv, pag_id))
     
+    conexao.commit()
     
+def insert_paypal(pagamento, pag_id):
+    conexao = sqlite3.connect('dados.db')
+    cursor = conexao.cursor()
     
+    cursor.execute('INSERT INTO Paypal (Email, Senha, Pagamento_ID) VALUES (?, ?, ?)', (pagamento.email, pagamento.get_senha(), pag_id))
     
+    conexao.commit()
     
-        
+def insert_trans_bancaria(pagamento, pag_id):
+    conexao = sqlite3.connect('dados.db')
+    cursor = conexao.cursor()
+    
+    cursor.execute('INSERT INTO Trans_Bancária (Código_Banco, Conta_Origem, Conta_Destino, Pagamento_ID) VALUES (?, ?, ?, ?)', (pagamento.codigo, pagamento.origem, pagamento.destino, pag_id))
+    
+    conexao.commit()
