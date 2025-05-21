0092e33866bfd8df96a1a067b729b1a00ccf1087
Elisa C Medeiros <pedro.melchiordeoliveira@gmail.com>
2025-05-21 10:19:19 -0400

    Update main.py

--- programação_orientada_a_objetos/atividade_avaliativa_16-05/main.py
+++ programação_orientada_a_objetos/atividade_avaliativa_16-05/main.py
@@ -1,11 +1,31 @@
 from modulo import *
 
 menu()
+cria_tabela()
 
-trans = cria_pagamento()
+clicou = False
+trans = cria_trans()
+pag_id = insert_pagamento_retorna_id(trans)
+
+
 
 if trans.metodo == 'Crédito':
-    credito = cria_credito()
-
-if trans.metodo == 'Paypal':
-    paypal = cria_paypal()
+    pagamento = cria_credito()
+    st.markdown('---')
+    if st.button("Confirmar pagamento"):
+        insert_credito(pagamento, pag_id)
+        st.write("✅ Pagamento Confirmado e Salvo!")
+    
+elif trans.metodo == 'Paypal':
+    pagamento = cria_paypal()
+    st.markdown('---')
+    if st.button("Confirmar pagamento"):
+        insert_paypal(pagamento, pag_id)
+        st.write("✅ Pagamento Confirmado e Salvo!")
+    
+elif trans.metodo == 'Transferência Bancária':
+    pagamento = cria_bancaria()
+    st.markdown('---')
+    if st.button("Confirmar pagamento"):
+        insert_trans_bancaria(pagamento, pag_id)
+        st.write("✅ Pagamento Confirmado e Salvo!")
