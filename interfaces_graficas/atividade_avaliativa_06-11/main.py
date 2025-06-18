3ede48b9d5b2156f72a43ac27382e266783ac447
PedroMeliveira <pedro.melchiordeoliveira@gmail.com>
2025-06-17 23:23:15 -0400

    commit

--- interfaces_graficas/atividade_avaliativa_06-11/main.py
+++ interfaces_graficas/atividade_avaliativa_06-11/main.py
@@ -3,3 +3,13 @@ from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication, QWidget
 from PyQt6.QtGui import QPixmap
 
+class Pexel(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("interfaces_graficas/atividade_avaliativa_06-11/teste.ui")
+
+if __name__ == "__main__":
+    app = QApplication([])
+    janela = Pexel()
+    janela.show()
+    app.exec()
